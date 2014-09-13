3bb834125af4f7010ec2fcfdabca4ad72f466c08
Hovl <hovl.swc@gmail.com>
2014-09-13 23:02:31 +0400

    If the file is directory do not get its name without extension

--- src/ebs/music/RenameYear.java
+++ src/ebs/music/RenameYear.java
@@ -25,27 +25,44 @@ public class RenameYear {
 		}
 
 		File[] subFiles = baseDirectory.listFiles();
+		if(subFiles == null) {
+			System.out.println(basePath + " is not a directory! Quiting...");
+			return;
+		}
+
 		for (File subFile : subFiles) {
-			int extPos = subFile.getName().lastIndexOf(".");
-			String fileNameWithoutExtension = extPos > 0 ? subFile.getName().substring(0, extPos) : subFile.getName();
+			System.out.println("File to rename: " + subFile.getName());
+
+			String fileNameWithoutExtension;
+			if(subFile.isDirectory()) {
+				fileNameWithoutExtension = subFile.getName();
+			} else {
+				int extPos = subFile.getName().lastIndexOf(".");
+				fileNameWithoutExtension =
+						extPos > 0 ? subFile.getName().substring(0, extPos) : subFile.getName();
+			}
+			System.out.println("File name without extension: " + fileNameWithoutExtension);
 
 			int qStartPos = fileNameWithoutExtension.lastIndexOf(" [");
 			if (qStartPos > 0) {
 				String quality = subFile.getName().substring(qStartPos, subFile.getName().length());
+				System.out.println("Quality: " + quality);
 
 				String fileNameWithoutExtensionAndQuality = fileNameWithoutExtension.substring(0, qStartPos);
+				System.out.println(" File name without extension and quality: " + fileNameWithoutExtensionAndQuality);
 
 				int yearPos = fileNameWithoutExtensionAndQuality.lastIndexOf(" [");
 				if (yearPos > 0) {
 					String year = fileNameWithoutExtensionAndQuality.substring(yearPos + 2,
 							fileNameWithoutExtensionAndQuality.length() - 1);
+					System.out.println("Year: " + year);
 
 					String fileNameWithoutExtAndQualityAndYear = fileNameWithoutExtensionAndQuality.substring(0,
 							yearPos);
+					System.out.println("File name without extension, quality and year: " + fileNameWithoutExtAndQualityAndYear);
 
 					String fileNewName =
 							fileNameWithoutExtAndQualityAndYear.replaceFirst(" - ", " - " + year + " - ") + quality;
-
 					System.out.println("New Name: " + fileNewName);
 
 					System.out.println("Renamed: " + subFile.renameTo(new File(basePath, fileNewName)));
